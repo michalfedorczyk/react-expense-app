7b79e29ab83fd6134f62e16bb403259220a1b5f1
Michal M. Fedorczyk <michalfedorczyk@gmail.com>
2020-10-30 13:26:03 +0100

    Added redux into play

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense, removeExpense, editExpense } from "./actions/expenses";
+import {
+  setTextFilter,
+  sortByAmount,
+  sortByDate,
+  setStartDate,
+  setEndDate,
+} from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: "Water bill", amount: 500 }));
+store.dispatch(addExpense({ description: "Gas bill", amount: 1500 }));
+store.dispatch(setTextFilter("bill"));
+
+const state = store.getState();
+const visibleExpanses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpanses);
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
