398581c88091d97f7d4f4f433e5e5453b0b173fd
Michal M. Fedorczyk <michalfedorczyk@gmail.com>
2020-11-03 06:06:08 +0100

    Setup dev dependencies and dev location

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devMode = process.env !== "production";
 
-const plugins = [new MiniCssExtractPlugin({filename: "styles.css"})];
+const plugins = [new MiniCssExtractPlugin({ filename: "styles.css" })];
 
 module.exports = (env) => {
   const isProduction = env === "production";
   return {
     entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, "public"),
+      path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js",
     },
     module: {
@@ -24,18 +24,18 @@ module.exports = (env) => {
           use: [
             MiniCssExtractPlugin.loader,
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               options: {
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
             {
-              loader: 'sass-loader',
+              loader: "sass-loader",
               options: {
-                sourceMap: true
-              }
-            }
-          ]
+                sourceMap: true,
+              },
+            },
+          ],
         },
       ],
     },
@@ -44,6 +44,7 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
+      publicPath: "/dist/",
     },
   };
 };
