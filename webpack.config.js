6d51c7eede487c6d529a35b5277f026e44ae45a2
Michal M. Fedorczyk <michalfedorczyk@gmail.com>
2020-11-04 07:06:11 +0100

    Created new Firebase project with other keys, moved .envs to config folder added to gitignore file.

--- webpack.config.js
+++ webpack.config.js
@@ -30,9 +30,9 @@ const plugins = [
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 if (process.env.NODE_ENV === "test") {
-  require("dotenv").config({ path: "./.env.test" });
+  require("dotenv").config({ path: "./config/.env.test" });
 } else if (process.env.NODE_ENV === "development") {
-  require("dotenv").config({ path: "./.env.development" });
+  require("dotenv").config({ path: "./config/.env.development" });
 }
 
 module.exports = (env) => {
