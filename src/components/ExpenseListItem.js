62493c9291d9c33f934586136363a25c9e7d9ba7
Michal M. Fedorczyk <michalfedorczyk@gmail.com>
2020-11-07 21:47:13 +0100

    Final version af the app_2

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -6,7 +6,7 @@ import numeral from "numeral";
 const ExpenseListItem = ({ id, description, note, amount, createdAt }) => (
   <Link className="list-item" to={`/edit/${id}`}>
     <div>
-      <h3 className="list-item__title">Description: {description}</h3>
+      <h3 className="list-item__title">{description}</h3>
       <span className="list-item__subtitle">
         {moment(createdAt).format("MMMM Do, YYYY")}
       </span>
